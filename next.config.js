6138863cfc3156e02e8fa0174376c5da07f75a69
front_김건우 <kim.geonu@cashwalk.io>
2024-12-17 10:35:23 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  async headers() {
+    return [
+      {
+        // 특정 경로에 대해 헤더를 추가합니다.
+        source: "/:path*", // 모든 경로에 대해 적용 (예: /about, /api/...)
+        headers: [
+          {
+            key: "ngrok-skip-browser-warning",
+            value: "true", // 헤더 값을 true로 설정
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string
-}
+const DELAY_TIME = 2000;
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req: any, res: any) {
+  // 100ms 뒤에 응답 반환
+  setTimeout(() => {
+    res
+      .status(200)
+      .json({ message: `This response was delayed by ${DELAY_TIME}ms` });
+  }, DELAY_TIME);
 }
